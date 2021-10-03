a20581c80a337f5e5b5799904caa77aed11bb182
Crac0erXD <66012875+Sergiu15155@users.noreply.github.com>
2021-10-03 23:08:38 +0300

    Added CheatLoverz-Remake via files
    im gay.

---
+++ README.md
@@ -0,0 +1,15 @@
+# CheatLoverz-REMAKE
+Remake of the 2018 cheatloverz.
+
+# Informations
+**CL Developer:** *Dylan777*
+
+**Remake Developer:** *masterpasta#0001*
+
+**Submitted by:** *masterpasta#0001*
+
+# ABOUT THIS
+This source is using as renderer **ImGui**
+
+> This source is compatible only with ImGui.
+> Use your own ImGui files.
---
+++ offsets.h
@@ -0,0 +1,36 @@
+namespace Offsets 
+{
+	uintptr_t OwningGameInstance = 0x188;
+	uintptr_t LocalPlayers = 0x38;
+	uintptr_t PlayerController = 0x30;
+	uintptr_t PlayerCameraManager = 0x2B8;
+	uintptr_t AcknowledgedPawn = 0x2A0;
+	uintptr_t Levels = 0x140;
+	uintptr_t PersistentLevel = 0x30;
+	uintptr_t AActors = 0x98;
+	uintptr_t ActorCount = 0xA0;
+	uintptr_t ComponentVelocity = 0x140;
+	uintptr_t RootComponent = 0x130;
+	uintptr_t FireStartLoc = 0x880;
+	uintptr_t RelativeLocation = 0x2E0;
+	uintptr_t RelativeRotation = 0x128;
+	uintptr_t CurrentWeapon = 0xAA0;
+	uintptr_t PlayerState = 0x368;
+	uintptr_t Mesh = 0x280;
+	uintptr_t TeamIndex = 0xED0;
+	uintptr_t ReloadTime = 0x10C;
+	uintptr_t ReloadScale = 0x110;
+	uintptr_t bDisableEquipAnimation = 0x28;
+	uintptr_t bADSWhileNotOnGround = 0x3CD1;
+	uintptr_t ReviveFromDBNOTime = 0x35E0;
+	uintptr_t CustomTimeDilation = 0x98;
+	uintptr_t LastFireTime = 0x9CC;
+	uintptr_t LastFireTimeVerified = 0x9D0;
+	uintptr_t RecoilHoriz = 0x214;
+	uintptr_t RecoilVert = 0x204;
+	uintptr_t RecoilDownsightsMultiplier = 0x230;
+	uintptr_t LastReloadTime = 0xA24;
+	uintptr_t TargetPawn = 0x8;
+	uintptr_t localplayerstate = 0x8;
+	uintptr_t LastSuccessfulReloadTime = 0xA28;
+}
